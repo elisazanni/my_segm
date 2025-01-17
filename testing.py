ead61ac0f2d33df0bb0a286902f1d85221d3d741
elisazanni <elisa.zanni.99@gmail.com>
2025-01-17 11:58:08 +0100

    name changed

--- testing.py
+++ testing.py
@@ -1,12 +1,10 @@
 import os
-import cv2
 import segmentation_models_pytorch as smp
 import torch
-from alive_progress import alive_bar
-from src.utils.semantic_masks import class2color
-import time
-import numpy as np
-from src.utils.albumentation import get_preprocessing
+import dataset_manager
+import glob
+from PIL import Image
+import sys
 from config import config
 
 IMAGE_HEIGHT = config['patch_size']
@@ -28,7 +26,7 @@ ENCODER = config['encoder']  # encoder
 ENCODER_WEIGHTS = config['encoder_weights']  # pretrained weights
 ACTIVATION = None
 CLASSES = classes
-DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
+DEVICE = config['device']
 MODEL_NAME = config['model_name']  # segmentation model
 
 # create segmentation architecture
@@ -61,11 +59,36 @@ elif MODEL_NAME == 'Unet++':
 else:
     model = None
 
-model.load_state_dict(torch.load(weights_path, map_location=torch.device('cuda')))
-model.to(DEVICE).eval()
+def predict_and_save(input_dir, output_dir):
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
 
-preprocessing_fn = smp.encoders.get_preprocessing_fn('timm-mobilenetv3_large_100', ENCODER_WEIGHTS)
-preprocessing = get_preprocessing(preprocessing_fn)
+    model.load_state_dict(torch.load(weights_path, map_location=torch.device(DEVICE)))
+    model.to(DEVICE).eval()
+
+    preprocessing_fn = smp.encoders.get_preprocessing_fn(config['encoder'], config['encoder_weights'])
+    preprocessing = dataset_manager.get_preprocessing(preprocessing_fn)
+
+    for file_name in os.listdir(input_dir):
+        if file_name.endswith('_0000.png'):
+            # Carica l'immagine
+            img_path = os.path.join(input_dir, file_name)
+            img = Image.open(img_path)
+            img_tensor = preprocessing(img).unsqueeze(0).to(DEVICE)
+
+            # Predici
+            with torch.no_grad():
+                output = model(img_tensor)
+                output = torch.argmax(output, dim=1).squeeze(0).cpu().numpy()
+
+            # Salva l'immagine predetta
+            pred_img = Image.fromarray(output.astype('uint8'))
+            pred_file_name = file_name.replace('_0000.png', '_pred.png')
+            pred_img.save(os.path.join(output_dir, pred_file_name))
 
 
 
+
+input_directory = sys.argv[2]
+output_directory = sys.argv[3]
+predict_and_save(input_directory, output_directory)
